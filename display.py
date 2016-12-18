3819e77581e6b4e6627079dfd81ba6972082e584
Alexander Simko <asimko94@gmail.com>
2016-12-18 19:51:43 +0100

    Code refractoring and documentation added

--- display.py
+++ display.py
@@ -1,16 +1,20 @@
 #! python
+"""
+This module is used for displaying and running a maze generation algorithm.
+This moduel uses pygame to create the graphical display.
+"""
 
 import pygame
 import generator
 import argparse
-
-pygame.init()
 
 RED = (255, 0, 0)
 GREEN = (0,255,0)
 BLUE = (0,0,255)
 BLACK = (0,0,0)
 WHITE = (255,255,255)
+
+# -- ALL VARIABLES ARE SET BY setup() FROM THE COMMAND LINE ARGUMENTS OR DEFAULT VALUES -- #
 
 WINDOW_SIZE = None
 SQUARE_SIZE = None
@@ -27,22 +31,25 @@ BACKTRACKED_COLOR = None
 SHOW_BACKTRACK = None
 
 def setup():
+	"""
+	Sets all global constants to their apropriate values, either a default value or one specified from the command line
+	"""
 	global WINDOW_SIZE, SQUARE_SIZE, FRAMERATE, BACKGROUND_COLOR, LINE_COLOR, UPDATES, CURRENT_COLOR, VISITED_COLOR, BACKTRACKED_COLOR, SHOW_BACKTRACK
 
 	parser = argparse.ArgumentParser(description="Randomly creates a maze with a given size")
-	parser.add_argument("-s", "--size", default = (800, 600),  help = "Specifyes the window size. Defalut is 800x600.", nargs = 2, type = int, metavar = ("width", "height"))
-	parser.add_argument("-S", "--square", default = 20, help = "Specifyes the squares' size. Default is 20.", type = int, metavar = "size")
-	parser.add_argument("-f", "--framerate", default = 60, help = "Specifyes the framerate. Default is 60 fps.", type = int, metavar ="fps")
-	parser.add_argument("-b", "--background", default = (32, 32, 32), help = "Specifyes the background color. Colors are given in RGB format. Defalut is (32, 32, 32)",
+	parser.add_argument("-s", "--size", default = (800, 600),  help = "Specifies the window size. Defalut is 800x600.", nargs = 2, type = int, metavar = ("width", "height"))
+	parser.add_argument("-S", "--square", default = 20, help = "Specifies the squares' size. Default is 20.", type = int, metavar = "size")
+	parser.add_argument("-f", "--framerate", default = 60, help = "Specifies the framerate. Default is 60 fps.", type = int, metavar ="fps")
+	parser.add_argument("-b", "--background", default = (32, 32, 32), help = "Specifies the background color. Colors are given in RGB format. Defalut is (32, 32, 32)",
 						nargs = 3, type = int, metavar = ("r", "g", "b"))
-	parser.add_argument("-u", "--update", default = 1, help = "Specifyes how oft the screen should update, i.e. a value of 1 means every frame, a value of 2 means every other frame and so on. Default is 1", type=int)
-	parser.add_argument("-l", "--line", default = WHITE, help = "Specifyes the color used for the lines. The color is given in RGB format. Default is %s" % (str(WHITE)),
+	parser.add_argument("-u", "--update", default = 1, help = "Specifies how oft the screen should update, i.e. a value of 1 means every frame, a value of 2 means every other frame and so on. Default is 1", type=int)
+	parser.add_argument("-l", "--line", default = WHITE, help = "Specifies the color used for the lines. The color is given in RGB format. Default is %s" % (str(WHITE)),
 						nargs = 3, type = int, metavar = ("r", "g", "b"))
-	parser.add_argument("-c", "--current", default = (0, 255, 0, 255), help = "Specifyes the color of the current position. The color is given in RGBA form. Default is (0, 255, 0, 0)",
+	parser.add_argument("-c", "--current", default = (0, 255, 0, 255), help = "Specifies the color of the current position. The color is given in RGBA form. Default is (0, 255, 0, 0)",
 						nargs = 4, type = int, metavar = ("r", "g", "b", "a"))
-	parser.add_argument("-v", "--visited", default = (255, 0, 255, 50), help = "Specifyes the color of the visited positions. The color is given in RGBA form. Default is (255, 0, 255, 50)",
+	parser.add_argument("-v", "--visited", default = (255, 0, 255, 50), help = "Specifies the color of the visited positions. The color is given in RGBA form. Default is (255, 0, 255, 50)",
 						nargs = 4, type = int, metavar = ("r", "g", "b", "a"))
-	parser.add_argument("-B", "--backtrack", default = (0, 128, 255, 50), help = "Specifyes the color of backtracked positions. The color is given in RGBA form. Default is (0, 128, 255, 50)",
+	parser.add_argument("-B", "--backtrack", default = (0, 128, 255, 50), help = "Specifies the color of backtracked positions. The color is given in RGBA form. Default is (0, 128, 255, 50)",
 						nargs = 4, type = int, metavar = ("r", "g", "b", "a"))
 	parser.add_argument("-nb", "--no-backtrack", action = "store_true", help = "Does not mark backtrackted positions with a different color")
 
@@ -60,9 +67,31 @@ def setup():
 	SHOW_BACKTRACK = not args.no_backtrack
 
 def clear_screen(display):
+	"""
+	Clears the given display by filling it with BACKGOUND_COLOR
+
+	Args:
+	dispalay	-	The display to clear
+
+	Types:
+	display 	-	pygame.Surface
+	"""
 	display.fill(BACKGROUND_COLOR)
 
 def run_generator(display, clock, grid):
+	"""
+	Runs the maze generation with the given display, clock, grid
+
+	Args:
+	display 	-	The display to display everyting on
+	clock 		-	The clock used for limiting the framerate
+	grid 		- 	The grid to generate the maze on
+
+	Types:
+	display 	-	pygame.Surface
+	clock 		- 	pygame.time.Clock
+	grid 		- 	[[generation.Cell]]
+	"""
 	frame = 0
 	for current in generator.make_maze(grid):
 		for event in pygame.event.get():
@@ -78,15 +107,18 @@ def run_generator(display, clock, grid):
 
 			pygame.display.update()
 		clock.tick(FRAMERATE)
+
 	for row in grid:
 		for cell in row:
 			cell.draw(display, LINE_COLOR, SQUARE_SIZE)
-
 
 	pygame.display.update()
 
 
 def init_vars():
+	"""
+	Initializes all of Cell's common variables based on the global variables
+	"""
 	generator.Cell.BACKGROUND_COLOR = BACKGROUND_COLOR
 	generator.Cell.VISITED_COLOR = VISITED_COLOR
 	generator.Cell.CURRENT_COLOR = CURRENT_COLOR
@@ -95,6 +127,10 @@ def init_vars():
 	
 
 def main():
+	"""
+	Main function of the program.
+	"""
+	pygame.init()
 	init_vars()
 	game_display = pygame.display.set_mode(WINDOW_SIZE)
 	pygame.display.set_caption("Maze Generator")
--- generator.py
+++ generator.py
@@ -1,20 +1,48 @@
+"""
+This module is for generating a random maze using a recursive backtracking
+algorithm and then display it on a pygame.Surface surface.
+"""
+
 import pygame
 import random
 
 def draw_rect_with_alpha(display, rgba, pos, size):
+	"""
+	Draws a rectangle at a given position on a given surface with a given size and rgba color.
+
+	Args:
+	display 	-	The surface to draw on
+	rgba 		- 	The rgba color, i.e. color given in a (Red, Green, Blue, Alpha) format.
+	pos 		- 	The position of the rectangle
+	size 		- 	The size of the rectangle
+
+	Types:
+	display 	-	pygame.Surface
+	rgba 		- 	tuple
+	pos 		-	tuple
+	size 		- 	tuple
+	"""
 	rect = pygame.Surface(size, flags = int("0x00010000", 16))
 	rect.fill(rgba)
 	display.blit(rect, pos)
 
 class Cell:
+	"""
+	A cell in a grid. Used for making a maze out of the grid.
+	Cell(row, col) -> Cell
+	"""
 
-	CURRENT_COLOR = None
-	VISITED_COLOR = None
-	BACKGROUND_COLOR = None
-	BACKTRACKED_COLOR = None
-	SHOW_BACKTRACK = None
+	# -- ALL VALUES SHOULD BE SET SOMEWHERE OUTSIDE. BY DEFAULT display.init_vars() WILL HANDLE THIS -- #
+	CURRENT_COLOR = (0,0,0)
+	VISITED_COLOR = (0,0,0)
+	BACKGROUND_COLOR = (0,0,0)
+	BACKTRACKED_COLOR = (0,0,0)
+	SHOW_BACKTRACK = False
 
 	def __init__(self, row, col):
+		"""
+		Initializes the Cell object
+		"""
 		self.row = row
 		self.col = col
 		self.visited = False
@@ -29,6 +57,20 @@ class Cell:
 		}
 
 	def draw(self, display, color, size):
+		"""
+		Draws the cell onto the given display with the given color and size if (and only if) the have been a change
+		sice the last call of draw. Otherwise this method has no effect. The color is a RGB color.
+
+		Args:
+		display		-	The surface to draw on
+		color 		- 	The color to use for drawing the lines around the cell
+		size 		- 	The size of the cell in pixels
+
+		Types:
+		display 	- 	pygame.Surface
+		color 		-	tuple
+		size		-	tuple
+		"""
 		to_compare = {
 			"walls": self.walls,
 			"current": self.current,
@@ -46,6 +88,20 @@ class Cell:
 
 
 	def __draw(self, display, color, size):
+		"""
+		Used internally for drawing the cell onto the display regardles of any change or not. This method
+		should not be called outside of Cell as it is very slow to call repeated times.
+
+		Args:
+		display		-	The surface to draw on
+		color 		- 	The color to use for drawing the lines around the cell
+		size 		- 	The size of the cell in pixels
+
+		Types:
+		display 	- 	pygame.Surface
+		color 		-	tuple
+		size		-	tuple
+		"""
 		if self.walls[0]: # up
 			pygame.draw.line(display, color, (self.col * size       , self.row * size)       , (self.col * size + size, self.row * size))
 		if self.walls[3]: # down
@@ -67,17 +123,53 @@ class Cell:
 		
 
 	def get_neighbours(self, grid):
+		"""
+		Yields all the neighbours of the cell within the given grid.
+
+		Args:
+		grid		-	The grid to yield the neighbours
+
+		Types:
+		grid		-	[[Cell]]
+
+
+		Yields		-	The neigbours of the cell in the given grid.
+		Yield type 	-	Cell
+		"""
 		for diff in ((-1, 0), (1, 0), (0, -1), (0, 1)):
 			res = Vector((self.row, self.col)) + diff
 			if res[0] >= 0 and res[1] >= 0 and res[0] < len(grid) and res[1] < len(grid[0]):
 				yield grid[res[0]][res[1]]
 
 	def get_unvisited_neighbours(self, grid):
+		"""
+		Yields all unvisited neighbours of the cell in the given grid.
+
+		Args:
+		grid 		-	The grid to yield the neighbours.
+
+		Types:
+		grid		-	[[Cell]]
+
+		Yields		-	The unvisited neighbours of the cell in the given grid.
+		Yield type 	-	Cell
+		"""
 		for neighbour in self.get_neighbours(grid):
 			if not neighbour.visited:
 				yield neighbour
 
 	def remove_wall(self, direction, other):
+		"""
+		Removes the wall between the cell and the other cell, 'other' in the direction 'direction'
+
+		Args:
+		direction	-	The direction of the other cell
+		other 		- 	The neighbouring cell in the direcion direction
+
+		Types:
+		direciton 	-	tuple, {(-1, 0), (1, 0), (0, -1), (0, 1)}
+		other 		- 	Cell
+		"""
 		d_map = {
 			(-1,  0): 3,
 			(1 ,  0): 0,
@@ -89,6 +181,10 @@ class Cell:
 
 
 class Vector(tuple):
+	"""
+	A basic vector that supports multiplication (only with Scalar), addition and subtraction.
+	Vector inherits from 'tuple' and therfore behaves it almost the same.
+	"""
 	def __mul__(self, other):
 		return Vector([e * other for e in self])
 
@@ -100,6 +196,25 @@ class Vector(tuple):
 
 
 def make_maze(grid):
+	"""
+	Runs the generation algorithm on the given grid, yeilding every time a new
+	position is choosen as the current position.
+	For more information on the algorithm in use see:
+	https://en.wikipedia.org/wiki/Maze_generation_algorithm#Recursive_backtracker
+
+
+	Args:
+	grid 		-	The grid to make the maze on.
+
+	Types:
+	grid		-	[[Cell]]
+
+
+	Yields		-	The current position of the algorithm, i.e. the position
+					that will be evaluated next acording to the rules of the
+					algorithm
+	Yield type 	-	Cell
+	"""
 	current = grid[0][0]
 	current.visited = True
 	current.current = True
@@ -113,22 +228,30 @@ def make_maze(grid):
 			stack.append(current)
 			current.remove_wall(Vector((current.row, current.col)) - (next_cell.row, next_cell.col), next_cell)
 			current.current = False
+
 			current = next_cell
 			current.visited = True
 			current.current = True
 			visited.append(current)
+
 		elif len(stack) > 0:
 			current.current = False
 			current.backtracked = True
+			
 			current = stack.pop(-1)
 			current.current = True
 			current.backtracked = True
+
 		yield current
+
 	while len(stack) > 0 and Cell.SHOW_BACKTRACK:
 		current.current = False
 		current.backtracked = True
+		
 		current = stack.pop(-1)
 		current.current = True
 		current.backtracked = True
+		
 		yield current
+	
 	current.current = False
